7dfb2461ae821144ddadd719fca510213c84d5a8
jalil <jalil@1001.digital>
2023-10-06 01:12:45 +0200

    Add color mode toggle

---
+++ lib/components/ToggleColorMode.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="() => toggleDark()">
+    <Icon v-if="mode === 'dark'" type="sun" :size="20" />
+    <Icon v-else type="moon" :size="20" />
+  </button>
+</template>
+
+<script setup>
+import { useColorMode } from '@vueuse/core'
+
+const mode = useColorMode() // Ref<'dark' | 'light'>
+
+const toggleDark = () => {
+  mode.value = mode.value === 'light' ? 'dark' : 'light'
+}
+</script>
