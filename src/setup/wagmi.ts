83e57f6004eca0da9c830b88b0984d346d0e28d6
jalil <jalil@1001.digital>
2023-11-15 23:53:01 +0100

    vvork in progress

--- src/setup/wagmi.ts
+++ src/setup/wagmi.ts
@@ -1,36 +1,13 @@
-import { mainnet } from '@wagmi/chains'
-import { createConfig, configureChains, InjectedConnector } from '@wagmi/core'
-import { MetaMaskConnector } from '@wagmi/core/connectors/metaMask'
-import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
-import { alchemyProvider } from '@wagmi/core/providers/alchemy'
-import { publicProvider } from '@wagmi/core/providers/public'
+import setup from '../../lib/setup-wagmi'
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
-  [
-    alchemyProvider({ apiKey: import.meta.env.VITE_ALCHEMY }),
-    publicProvider(),
-  ],
-  {
-    batch: { multicall: true },
-    pollingInterval: 10_000,
-  }
-)
-
-const wagmi = createConfig({
-  autoConnect: true,
-  connectors: [
-    new InjectedConnector({ chains }),
-    new MetaMaskConnector({ chains }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
-      },
-    }),
-  ],
+const {
+  chains,
   publicClient,
   webSocketPublicClient,
+  wagmi
+} = setup({
+  alchemyApiKey: import.meta.env.VITE_ALCHEMY,
+  walletConnectProjectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
 })
 
 export {
